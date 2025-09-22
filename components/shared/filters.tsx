0ea96595ec146e1eed9178a4a0e5e0840cc5b329
Артём Романнко <romanenkomj@gmail.com>
2025-09-22 21:09:22 +0300

    add any

---
+++ components/shared/filters.tsx
@@ -0,0 +1,50 @@
+'use client'
+import React from 'react'
+import { Title } from './title'
+import { FilterCheckbox, RangeSlider } from '@/components/shared/index'
+import { Input } from '../ui'
+import { CheckboxFiltersGroup } from './checkbox-filters-grop'
+import { useFilterIngredients } from '@/hooks/useFilterIngredients'
+interface Props {
+  className?: string
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  const {ingredients} = useFilterIngredients()
+	
+const items = ingredients.map((ingredient) => ({text: ingredient.name, value: ingredient.id.toString()}))
+
+
+
+	return (
+    <div className={className}>
+      <Title text='Фильтры' size='sm' className='mb-5 font-bold' />
+
+      {/* Верхние чекбоксы */}
+      <div className='flex flex-col gap-4'>
+        <FilterCheckbox text='Можно собирать' value='1' />
+        <FilterCheckbox text='Новинки' value='2' />
+      </div>
+
+      {/* Фильтр цен */}
+
+      <div className='mt-5 border-y border-y-neutral-100 py-6 pb-7'>
+        <p className='font-bold mb-3'>Цена от и до:</p>
+        <div className='flex gap-3 mb-5'>
+          <Input type='number' placeholder='0' min={0} max={1000} defaultValue={0} />
+          <Input type='number' placeholder='1000' min={100} max={1000} />
+        </div>
+        <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
+      </div>
+
+      {/* Фильтр ингредиентов */}
+      <CheckboxFiltersGroup
+        title='Ингредиенты'
+        className='mt-5'
+        limit={6}
+        defaultItems={items.slice(0, 6)}
+        items={items}
+      />
+    </div>
+  )
+}
